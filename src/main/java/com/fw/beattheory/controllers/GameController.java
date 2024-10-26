570a35fdf6d8bc2e5298aeda6afe9fd09c518fbc
moonspiner <134141903+moonspiner@users.noreply.github.com>
2024-10-26 11:35:50 -0400

    Update GameController.java

--- src/main/java/com/fw/beattheory/controllers/GameController.java
+++ src/main/java/com/fw/beattheory/controllers/GameController.java
@@ -24,7 +24,7 @@ import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/api/v1/game/")
-@CrossOrigin(origins = "https://beattheorymusic.com")
+@CrossOrigin(origins = {"https://beattheorymusic.com", "https://www.beattheorymusic.com"})
 public class GameController {
     private final GameService gameService;
     private static final Logger logger = (Logger) LoggerFactory.getLogger(GameController.class);
