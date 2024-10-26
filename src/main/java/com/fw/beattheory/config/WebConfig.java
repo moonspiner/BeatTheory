9e03ebc10b0e796de055d832d23579ed0ba19000
moonspiner <134141903+moonspiner@users.noreply.github.com>
2024-10-26 11:21:16 -0400

    Update WebConfig.java
    Update WebConfig.java

--- src/main/java/com/fw/beattheory/config/WebConfig.java
+++ src/main/java/com/fw/beattheory/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://beattheorymusic.com") // Specify the allowed origin
+                .allowedOrigins("https://beattheorymusic.com", "https://beattheorymusic.com") // Specify the allowed origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
